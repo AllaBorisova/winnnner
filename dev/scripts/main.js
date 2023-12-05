7f3ffce086df113eaaf3fcd08ebef421bee0d030
Alla Borisova <alla.borisova27@gmail.com>
2023-12-05 13:18:45 +0500

    feat:add buttons

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,45 +1,67 @@
-// JavaScript Document
-function preloadImagesFromDirectory(dir) {
-  if (!dir) return;
-  function getJSON(URL, success) {
-    // Create new function (within global namespace) (With unique name):
-    var uniqueID = 'json' + +new Date();
-    window[uniqueID] = function (data) {
-      success && success(data);
-    };
+// // JavaScript Document
+// function preloadImagesFromDirectory(dir) {
+//   if (!dir) return;
+//   function getJSON(URL, success) {
+//     // Create new function (within global namespace) (With unique name):
+//     var uniqueID = 'json' + +new Date();
+//     window[uniqueID] = function (data) {
+//       success && success(data);
+//     };
 
-    // Append new SCRIPT element to DOM:
-    document.getElementsByTagName('body')[0].appendChild(
-      (function () {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = URL.replace('callback=?', 'callback=' + uniqueID);
-        return script;
-      })()
+//     // Append new SCRIPT element to DOM:
+//     document.getElementsByTagName('body')[0].appendChild(
+//       (function () {
+//         var script = document.createElement('script');
+//         script.type = 'text/javascript';
+//         script.src = URL.replace('callback=?', 'callback=' + uniqueID);
+//         return script;
+//       })()
+//     );
+//   }
+
+//   function preload(srcArray) {
+//     for (var i = 0; i < srcArray.length; i++) {
+//       new Image().src = srcArray[i];
+//     }
+//   }
+
+//   // Get that JSON data:
+//   getJSON(
+//     'scanImageDirectory.json.php?directory=' +
+//       encodeURIComponent(dir) +
+//       '&callback=?',
+//     function (data) {
+//       return data.images ? preload(data.images) : false;
+//     }
+//   );
+// }
+
+// window.onload = function () {
+//   preloadImagesFromDirectory('img/');
+// };
+
+//buttons
+const callToActionBtns = document.querySelectorAll('.btn-rights');
+
+callToActionBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    callToActionBtns.forEach((f) =>
+      f != e.target ? f.classList.remove('active') : ''
     );
-  }
+    e.target.classList.toggle('active');
+  });
+});
 
-  function preload(srcArray) {
-    for (var i = 0; i < srcArray.length; i++) {
-      new Image().src = srcArray[i];
-    }
-  }
+const callToActionBtns2 = document.querySelectorAll('.btn-games');
 
-  // Get that JSON data:
-  getJSON(
-    'scanImageDirectory.json.php?directory=' +
-      encodeURIComponent(dir) +
-      '&callback=?',
-    function (data) {
-      return data.images ? preload(data.images) : false;
-    }
-  );
-}
-
-window.onload = function () {
-  preloadImagesFromDirectory('img/');
-};
-
+callToActionBtns2.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    callToActionBtns2.forEach((f) =>
+      f != e.target ? f.classList.remove('active') : ''
+    );
+    e.target.classList.toggle('active');
+  });
+});
 //timer
 function updateClock() {
   const currentTime = new Date();
